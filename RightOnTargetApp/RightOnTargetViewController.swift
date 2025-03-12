f24aaa1c5991cfce577cb3075dfe62f819d40e86
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-11 21:14:17 -0700

    1. Elements added and positioned

--- RightOnTargetApp/RightOnTargetViewController.swift
+++ RightOnTargetApp/RightOnTargetViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class RightOnTargetViewController: UIViewController {
+final class RightOnTargetViewController: UIViewController {
+    
+    // MARK: - @IBOutlet
+    
+    @IBOutlet var desiredValueLabel: UILabel!
+    
+    @IBOutlet var desiredValueSlider: UISlider!
+    
+    @IBOutlet var checkValueButton: UIButton!
+    
+    
+    // MARK: - Private properties
 
+    
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getButtonSettings()
+    }
+    
+    
+    // MARK: - IBAction
+    
+    
+    
+    
+    // MARK: - Private methods
+    
+    private func getLabelSettings(for label: UILabel) {
+        
     }
 
+    private func getButtonSettings() {
+        checkValueButton.layer.cornerRadius = 15
+        checkValueButton.layer.borderWidth = 2.5
+        checkValueButton.layer.borderColor = UIColor.white.cgColor
+        checkValueButton.backgroundColor = .black
+        checkValueButton.tintColor = .white
+        checkValueButton.setTitle("Check value", for: .normal)
+    }
+    
+    
 
 }
 
