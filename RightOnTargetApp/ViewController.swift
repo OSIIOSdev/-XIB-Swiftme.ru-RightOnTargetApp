da0ca31d597b725ba19eee49242740bc45f1aff8
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-11 18:53:32 -0700

    Initial Commit

---
+++ RightOnTargetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTargetApp
+//
+//  Created by OSIIOSdev on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
