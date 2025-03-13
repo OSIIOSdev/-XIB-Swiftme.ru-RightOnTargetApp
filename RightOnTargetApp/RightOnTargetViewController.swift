faa2ec840f18b535e44c2ccf21d09c622a3dfec0
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-13 14:39:29 -0700

    1. Business logic is implemented

--- RightOnTargetApp/RightOnTargetViewController.swift
+++ RightOnTargetApp/RightOnTargetViewController.swift
@@ -19,7 +19,13 @@ final class RightOnTargetViewController: UIViewController {
     
     // MARK: - Private properties
 
+    private var value = 0
+    private var round = 0
+    private var points = 0
+    private var result = 0
+    private let maxRoundValue = 10
     
+    private var missingValueText = "The value is still missing"
     
     // MARK: - View Lifecycle
     
@@ -36,19 +42,38 @@ final class RightOnTargetViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
         
-        showAlertController(title: "New game", message: "Try to get less than 15 points. To start a game press \"Start game\"")
+        showAlertController(title: "New game", message: "Try to get less than \(maxRoundValue) points. To start a game press \"Start game\"")
     }
 
     // MARK: - IBAction
     
-    @IBAction func sliderValueChanged() {
-        desiredValueLabel.text = String(format: "%.f", desiredValueSlider.value)
-    }
-    
-    @IBAction func checkValueButtonPressed() {
-        if checkValueButton.titleLabel?.text == "Start game" {
-            checkValueButton.setTitle( "Check", for: .normal)
+    @IBAction func getResultButton() {
+        switch round {
+        case 0:
+            value = Int.random(in: Int(desiredValueSlider.minimumValue)...Int(desiredValueSlider.maximumValue))
+            desiredValueLabel.text = value.formatted()
+            
+
+            checkValueButton.setTitle("Check value", for: .normal)
+            round = 1
+        case 1 ..< 5:
+            points = abs(value - Int(desiredValueSlider.value))
+            result += points
+        
+            value = Int.random(in: Int(desiredValueSlider.minimumValue)...Int(desiredValueSlider.maximumValue))
+            desiredValueLabel.text = value.formatted()
+        
+            round += 1
+        default:
+            result <= maxRoundValue ? showAlertController(title: "Game over", message: "You win! Your final result is \(result)") :
+                           showAlertController(title: "Game over", message: "You lose! Your final result is \(result)")
+            round = 0
+            result = 0
+            desiredValueLabel.text = 0.formatted()
+            checkValueButton.setTitle("Start game", for: .normal)
+            desiredValueLabel.text = missingValueText
         }
+        
     }
     
     
@@ -58,14 +83,14 @@ final class RightOnTargetViewController: UIViewController {
         
         desiredValueLabel.textColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         desiredValueLabel.font = UIFont.boldSystemFont(ofSize: 22)
-        desiredValueLabel.text = String(format: "%.f", desiredValueSlider.value)
+        desiredValueLabel.text = missingValueText
         
     }
 
     private func getSliderSettings() {
         
         desiredValueSlider.minimumValue = 0
-        desiredValueSlider.maximumValue = 1000
+        desiredValueSlider.maximumValue = 50
         desiredValueSlider.value = Float.random(in: desiredValueSlider.minimumValue...desiredValueSlider.maximumValue)
         
         desiredValueSlider.tintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
