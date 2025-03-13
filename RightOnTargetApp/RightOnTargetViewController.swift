54ef4164a6e5ba64410dff26e7a08fb2d629836e
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-13 13:56:55 -0700

    1. Minor changes added

--- RightOnTargetApp/RightOnTargetViewController.swift
+++ RightOnTargetApp/RightOnTargetViewController.swift
@@ -12,7 +12,6 @@ final class RightOnTargetViewController: UIViewController {
     // MARK: - @IBOutlet
     
     @IBOutlet var desiredValueLabel: UILabel!
-    
     @IBOutlet var desiredValueSlider: UISlider!
     
     @IBOutlet var checkValueButton: UIButton!
@@ -27,31 +26,74 @@ final class RightOnTargetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        
+        getSliderSettings()
+        getLabelSettings()
         getButtonSettings()
     }
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        
+        showAlertController(title: "New game", message: "Try to get less than 15 points. To start a game press \"Start game\"")
+    }
+
     // MARK: - IBAction
     
+    @IBAction func sliderValueChanged() {
+        desiredValueLabel.text = String(format: "%.f", desiredValueSlider.value)
+    }
     
+    @IBAction func checkValueButtonPressed() {
+        if checkValueButton.titleLabel?.text == "Start game" {
+            checkValueButton.setTitle( "Check", for: .normal)
+        }
+    }
     
     
     // MARK: - Private methods
     
-    private func getLabelSettings(for label: UILabel) {
+    private func getLabelSettings() {
+        
+        desiredValueLabel.textColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        desiredValueLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        desiredValueLabel.text = String(format: "%.f", desiredValueSlider.value)
         
     }
 
-    private func getButtonSettings() {
-        checkValueButton.layer.cornerRadius = 15
-        checkValueButton.layer.borderWidth = 2.5
-        checkValueButton.layer.borderColor = UIColor.white.cgColor
-        checkValueButton.backgroundColor = .black
-        checkValueButton.tintColor = .white
-        checkValueButton.setTitle("Check value", for: .normal)
+    private func getSliderSettings() {
+        
+        desiredValueSlider.minimumValue = 0
+        desiredValueSlider.maximumValue = 1000
+        desiredValueSlider.value = Float.random(in: desiredValueSlider.minimumValue...desiredValueSlider.maximumValue)
+        
+        desiredValueSlider.tintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        desiredValueSlider.thumbTintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        desiredValueSlider.minimumTrackTintColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        desiredValueSlider.maximumTrackTintColor = .white
+        
     }
     
+    private func getButtonSettings() {
+        
+        checkValueButton.layer.cornerRadius = 15
+        checkValueButton.layer.borderWidth = 2.5
+        checkValueButton.layer.borderColor = UIColor.black.cgColor
+        checkValueButton.backgroundColor = .white
+        checkValueButton.setTitleColor(.black, for: .normal)
+        checkValueButton.setTitle("Start game", for: .normal)
+        checkValueButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        
+    }
     
-
+    private func showAlertController(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alertController.addAction(okAction)
+        present(alertController, animated: true)
+    }
 }
 
